16018c7368023ec491d21cbbbdac10f05547c722
vakati <vijaybabu@gmail.com>
2024-08-12 15:18:33 +0530

    added files

--- src/constants/api_constants.py
+++ src/constants/api_constants.py
@@ -5,7 +5,7 @@ class APIConstants(object):
     def base_url(self):
         return "https://restful-booker.herokuappp.com"
     def url_create_booking(self):
-        return "https://restful-booker.herokuappp.com/booking"
+        return "https://restful-booker.herokuapp.com/booking"
     def url_create_token(self):
         return "https://restful-booker.herokuappp.com/auth"
     def url_patch_put_delete(self):
--- tests/crud/test_create_booking.py
+++ tests/crud/test_create_booking.py
@@ -1,5 +1,13 @@
 import allure
 import pytest
+import requests
+
+from src.constants.helpers.api_requests_wrapper import post_request
+from src.constants.api_constants import APIConstants
+from src.constants.helpers.payload_manager import payload_create_booking
+from src.constants.helpers.common_verification import *
+from utils.utils import Utils
+
 
 class TestCreateBooking(object):
 
@@ -7,7 +15,13 @@ class TestCreateBooking(object):
     @allure.title("Verify that booking status and booking id shouldn't be null")
     @allure.description("create a booking from the payload and verify that booking id should not be null ")
     def test_create_booking_positive(self):
-        pass
+        response = post_request(
+            url =  APIConstants().url_create_booking(),
+            auth = None,
+            headers = Utils().common_headers_json(),
+            payload = payload_create_booking(),
+            in_json = False
+        )
+        verify_http_status_code(response_data = response, expect_data = 200)
+        verify_json_key_for_not_null(response.json()['bookingid'])
 
-
-    
